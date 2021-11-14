618881cc2bafe93b46fcd5a4d687a237bc7131af
krystianpetek <krystianpetek2@gmail.com>
2021-11-14 20:02:10 +0100

    adapter zrobiont

--- adapter/Program.cs
+++ adapter/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Xml;
+using System.IO;
 
 namespace WzorzecAdapter
 {
@@ -20,7 +21,20 @@ namespace WzorzecAdapter
     }
 
     // tu trzeba dopisać klasę zwracającą zawartość pliku csv w postaci stringa (jednego długiego, rozdzielanego znakami nowego wiersza)
-
+    public class CsvApi
+    {
+        FileStream fs { get; set; }
+        StreamReader sr { get; set; }
+        private string wczytanyPlik;
+        public CsvApi()
+        {
+            fs = new FileStream("users.csv", FileMode.OpenOrCreate);
+            sr = new StreamReader(fs);
+            wczytanyPlik = sr.ReadToEnd();
+            sr.Close();
+        }
+        public string Odczyt() => wczytanyPlik;
+    }
 
     public interface IUserRepository
     {
@@ -62,6 +76,27 @@ namespace WzorzecAdapter
 
     }
 
+    public class CsvRepositoryAdapter : IUserRepository
+    {
+        private CsvApi _adapter = null;
+        public CsvRepositoryAdapter(CsvApi adapter)
+        {
+            _adapter = adapter;
+        }
+        public List<string> GetUserNames()
+        {
+            List<string> userNames = new List<string>();
+            string plik = _adapter.Odczyt();
+            var linia = plik.Split("\n");
+            foreach (var podzial in linia)
+            {
+                var imieNazwisko = podzial.Split(",");
+                userNames.Add(imieNazwisko[0] + " " + imieNazwisko[1]);                
+            }
+               
+            return userNames;
+        }
+    }
     // tu trzeba dopisać własny adapter implementujący odpowiedni interfejs
 
     public class Program
@@ -70,17 +105,17 @@ namespace WzorzecAdapter
         static void Main(string[] args)
         {
 
-            UsersApi userRepository = new UsersApi();
+            //UsersApi userRepository = new UsersApi();
 
-            IUserRepository adapter = new UsersRepositoryAdapter(userRepository);
+            //IUserRepository adapter = new UsersRepositoryAdapter(userRepository);
 
-            List<string> users = adapter.GetUserNames();
-            foreach (var userName in users)
-            {
-                Console.WriteLine(userName);
-            }
+            //List<string> users = adapter.GetUserNames();
+            //foreach (var userName in users)
+            //{
+            //    Console.WriteLine(userName);
+            //}
 
-            // var values = "name,surname".Split(",");
+            //var values = "name,surname".Split(",");
 
             // foreach(var val in values)
             // {
@@ -88,6 +123,18 @@ namespace WzorzecAdapter
             // }
 
             // TODO: wyświetl w konsoli
+
+            List<string> users2 = new List<string>();
+            CsvApi csvApi = new CsvApi();
+            IUserRepository adapter2 = new CsvRepositoryAdapter(csvApi);
+            users2 = adapter2.GetUserNames();
+            int licznik = 0;
+            foreach (var userName in users2)
+            {
+                Console.WriteLine($"{++licznik}. "+ userName);
+            }
+
+
         }
 
     }
---
+++ most/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace most
+{ 
+	public interface ITelewizor
+	{
+		int Kanal { get; set; }
+		void ZmienKanal(int kanal);
+	}
+
+	public class TvLg : ITelewizor
+	{
+
+		public TvLg()
+		{
+			this.Kanal = 1;
+		}
+
+		public int Kanal { get; set; }
+
+		public void Wlacz()
+		{
+			Console.WriteLine("Telewizor LG włączony.");
+		}
+
+		public void Wylacz()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ZmienKanal(int kanal)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+	public abstract class PilotAbstrakcyjny
+	{
+
+		private ITelewizor tv;
+
+		public PilotAbstrakcyjny(ITelewizor tv)
+		{
+			this.tv = tv;
+		}
+
+		public void ZmienKanal(int kanal)
+		{
+			tv.ZmienKanal(kanal);
+		}
+
+		public abstract void DoWlacz();
+
+	}
+
+	public class PilotHarmony : PilotAbstrakcyjny
+	{
+
+		public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+		public override void DoWlacz()
+		{
+			Console.WriteLine("Pilot Harmony włącza telewizor...");
+		}
+
+		public void DoWylacz()
+		{
+
+		}
+	}
+
+
+
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+
+			ITelewizor tv = new TvLg();
+			PilotAbstrakcyjny pilotHarmony = new PilotHarmony(tv);
+
+			pilotHarmony.DoWlacz();
+			pilotHarmony.DoWylacz();
+			Console.WriteLine("Kanał: " + tv.Kanal);
+
+		}
+	}
+}
+
+//using System;
+
+
+//public interface ITelewizor
+//{
+//	int Kanal { get; set; }
+//	void ZmienKanal(int kanal);
+//}
+
+//public class TvLg : ITelewizor
+//{
+
+//	public TvLg()
+//	{
+//		this.Kanal = 1;
+//	}
+
+//	public int Kanal { get; set; }
+
+//	public void Wlacz()
+//	{
+//		Console.WriteLine("Telewizor LG włączony.");
+//	}
+
+//	public void Wylacz()
+//	{
+//		throw new NotImplementedException();
+//	}
+
+//	public void ZmienKanal(int kanal)
+//	{
+//		throw new NotImplementedException();
+//	}
+
+//}
+
+//public abstract class PilotAbstrakcyjny
+//{
+
+//	private ITelewizor tv;
+
+//	public PilotAbstrakcyjny(ITelewizor tv)
+//	{
+//		this.tv = tv;
+//	}
+
+//	public void ZmienKanal(int kanal)
+//	{
+//		tv.ZmienKanal(kanal);
+//	}
+
+//	public abstract void DoWlacz();
+
+//}
+
+//public class PilotHarmony : PilotAbstrakcyjny
+//{
+
+//	public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+//	public override void DoWlacz()
+//	{
+//		Console.WriteLine("Pilot Harmony włącza telewizor...");
+//	}
+
+//	public void DoWylacz()
+//	{
+
+//	}
+//}
+
+
+
+//class Program
+//{
+//	public static void Main(string[] args)
+//	{
+
+//		ITelewizor tv = new TvLg();
+//		PilotAbstrakcyjny pilotHarmony = new PilotHarmony(tv);
+
+//		pilotHarmony.DoWlacz();
+//		pilotHarmony.DoWylacz();
+//		Console.WriteLine("Kanał: " + tv.Kanal);
+
+//	}
+//}
