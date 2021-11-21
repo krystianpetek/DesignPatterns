688a3cdc5c4c2014c5182815ef45e5d72ad73905
krystianpetek <krystianpetek2@gmail.com>
2021-11-21 12:25:31 +0100

    Update Program.cs

--- most/Program.cs
+++ most/Program.cs
@@ -30,27 +30,25 @@ public class TvLg : ITelewizor
 
 	public void Wylacz()
 	{
-		//
-		//
+		Console.WriteLine("Telewizor LG wyłączony.");
 	}
 
 	public void ZmienKanal(int kanal)
 	{
-		//
-		//
+		Kanal = kanal;
+        Console.WriteLine($"Telewizor LG, kanał {Kanal}");
 	}
-
 }
 
 
 
-public class TvXiaomi : ITelewizor
-{
+//public class TvXiaomi : ITelewizor
+//{
 
-	//
-	//
+//	//
+//	//
 
-}
+//}
 
 
 
@@ -118,7 +116,9 @@ class MainClass
 		ITelewizor tv = new TvLg();
 		//
 		//
-
+		PilotAbstrakcyjny pilotHarmony = new PilotHarmony(tv);
+		PilotAbstrakcyjny pilotPhilips = new PilotHarmony(tv);
+		
 		pilotHarmony.DoWlacz();
 		Console.WriteLine("Kanał: " + tv.Kanal);
 		pilotPhilips.DoZmienKanal(100);
@@ -131,16 +131,26 @@ class MainClass
 
 
 
+
+
+
+
+
+
+
+
+
+
 //using System;
 
 
 //public interface ITelewizor
 //{
 
-//    int Kanal { get; set; }
-//    //
-//    //
-//    void ZmienKanal(int kanal);
+//	int Kanal { get; set; }
+//	//
+//	//
+//	void ZmienKanal(int kanal);
 
 //}
 
@@ -149,29 +159,29 @@ class MainClass
 //public class TvLg : ITelewizor
 //{
 
-//    public TvLg()
-//    {
-//        this.Kanal = 1;
-//    }
+//	public TvLg()
+//	{
+//		this.Kanal = 1;
+//	}
 
-//    public int Kanal { get; set; }
+//	public int Kanal { get; set; }
 
-//    public void Wlacz()
-//    {
-//        Console.WriteLine("Telewizor LG włączony.");
-//    }
+//	public void Wlacz()
+//	{
+//		Console.WriteLine("Telewizor LG włączony.");
+//	}
 
-//    public void Wylacz()
-//    {
-//        //
-//        //
-//    }
+//	public void Wylacz()
+//	{
+//		//
+//		//
+//	}
 
-//    public void ZmienKanal(int kanal)
-//    {
-//        //
-//        //
-//    }
+//	public void ZmienKanal(int kanal)
+//	{
+//		//
+//		//
+//	}
 
 //}
 
@@ -180,8 +190,8 @@ class MainClass
 //public class TvXiaomi : ITelewizor
 //{
 
-//    //
-//    //
+//	//
+//	//
 
 //}
 
@@ -190,22 +200,22 @@ class MainClass
 //public abstract class PilotAbstrakcyjny
 //{
 
-//    private ITelewizor tv;
+//	private ITelewizor tv;
 
-//    public PilotAbstrakcyjny(ITelewizor tv)
-//    {
-//        //
-//        //
-//    }
+//	public PilotAbstrakcyjny(ITelewizor tv)
+//	{
+//		//
+//		//
+//	}
 
-//    //
-//    //
+//	//
+//	//
 
-//    public void ZmienKanal(int kanal)
-//    {
-//        //
-//        //
-//    }
+//	public void ZmienKanal(int kanal)
+//	{
+//		//
+//		//
+//	}
 
 //}
 
@@ -214,30 +224,30 @@ class MainClass
 //public class PilotHarmony : PilotAbstrakcyjny
 //{
 
-//    public PilotHarmony(ITelewizor tv) : base(tv) { }
+//	public PilotHarmony(ITelewizor tv) : base(tv) { }
 
-//    public void DoWlacz()
-//    {
-//        Console.WriteLine("Pilot Harmony włącza telewizor...");
-//        Wlacz();
-//    }
+//	public void DoWlacz()
+//	{
+//		Console.WriteLine("Pilot Harmony włącza telewizor...");
+//		Wlacz();
+//	}
 
-//    //
-//    //
+//	//
+//	//
 
 //}
 
 //public class PilotPhilips : PilotAbstrakcyjny
 //{
 
-//    //
-//    //
+//	//
+//	//
 
-//    public void DoWylacz()
-//    {
-//        Console.WriteLine("Pilot Philips wyłącza telewizor...");
-//        Wylacz();
-//    }
+//	public void DoWylacz()
+//	{
+//		Console.WriteLine("Pilot Philips wyłącza telewizor...");
+//		Wylacz();
+//	}
 
 //}
 
@@ -245,18 +255,126 @@ class MainClass
 
 //class MainClass
 //{
-//    public static void Main(string[] args)
-//    {
+//	public static void Main(string[] args)
+//	{
 
-//        ITelewizor tv = new TvLg();
-//        //
-//        //
+//		ITelewizor tv = new TvLg();
+//		//
+//		//
 
-//        pilotHarmony.DoWlacz();
-//        Console.WriteLine("Kanał: " + tv.Kanal);
-//        pilotPhilips.DoZmienKanal(100);
-//        Console.WriteLine("Kanał: " + tv.Kanal);
-//        pilotHarmony.DoWylacz();
+//		pilotHarmony.DoWlacz();
+//		Console.WriteLine("Kanał: " + tv.Kanal);
+//		pilotPhilips.DoZmienKanal(100);
+//		Console.WriteLine("Kanał: " + tv.Kanal);
+//		pilotHarmony.DoWylacz();
 
-//    }
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+
+
+//public interface ITelewizor
+//{
+//	int Kanal { get; set; }
+//	void ZmienKanal(int kanal);
+//}
+
+//public class TvLg : ITelewizor
+//{
+
+//	public TvLg()
+//	{
+//		this.Kanal = 1;
+//	}
+
+//	public int Kanal { get; set; }
+
+//	public void Wlacz()
+//	{
+//		Console.WriteLine("Telewizor LG włączony.");
+//	}
+
+//	public void Wylacz()
+//	{
+//		throw new NotImplementedException();
+//	}
+
+//	public void ZmienKanal(int kanal)
+//	{
+//		throw new NotImplementedException();
+//	}
+
+//}
+
+//public abstract class PilotAbstrakcyjny
+//{
+
+//	private ITelewizor tv;
+
+//	public PilotAbstrakcyjny(ITelewizor tv)
+//	{
+//		this.tv = tv;
+//	}
+
+//	public void ZmienKanal(int kanal)
+//	{
+//		tv.ZmienKanal(kanal);
+//	}
+
+//	public abstract void DoWlacz();
+
+//}
+
+//public class PilotHarmony : PilotAbstrakcyjny
+//{
+
+//	public PilotHarmony(ITelewizor tv) : base(tv) { }
+
+//	public override void DoWlacz()
+//	{
+//		Console.WriteLine("Pilot Harmony włącza telewizor...");
+//	}
+
+//	public void DoWylacz()
+//	{
+
+//	}
+//}
+
+
+
+//class Program
+//{
+//	public static void Main(string[] args)
+//	{
+
+//		ITelewizor tv = new TvLg();
+//		PilotAbstrakcyjny pilotHarmony = new PilotHarmony(tv);
+
+//		pilotHarmony.DoWlacz();
+//		pilotHarmony.DoWylacz();
+//		Console.WriteLine("Kanał: " + tv.Kanal);
+
+//	}
 //}
