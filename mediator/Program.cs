b3c78192811ad3d609073eaa945b442b11ece4cf
krystianpetek <krystianpetek2@gmail.com>
2021-12-08 21:26:28 +0100

    dodanie nowych

---
+++ mediator/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+//
+//
+//
+
+public class Mediator : IMediator
+{
+    List<IUzytkownik> uzytkownicy;
+
+    public Mediator()
+    {
+        uzytkownicy = new List<IUzytkownik>();
+    }
+
+    public void DodajUzytkownika(IUzytkownik uzytkownik)
+    {
+        //
+        //
+    }
+
+    public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca)
+    {
+        //
+        //
+        //
+    }
+}
+
+//
+//
+//
+
+public class Dev : IUzytkownik
+{
+    string login;
+    IMediator Mediator;
+
+    public Dev(IMediator Mediator, string login)
+    {
+        this.login = login;
+        this.Mediator = Mediator;
+    }
+
+    public void WyslijWiadomosc(string wiadomosc)
+    {
+        Mediator.WyslijWiadomosc(wiadomosc, this);
+    }
+
+    public void OdbierzWiadomosc(string wiadomosc)
+    {
+        Console.WriteLine("Programista " + login + " otrzymal wiadomosc: " + wiadomosc);
+    }
+}
+
+//
+//
+//
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //
+
+        IUzytkownik ania = new Klient(mediator, "Ania");
+        //
+        //
+
+        //
+        mediator.DodajUzytkownika(nakamoto);
+        //
+
+        //
+        geohot.WyslijWiadomosc("Czekam az Nakamoto zaparzy kawe...");
+
+    }
+}
+
+
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+
+
+////
+////
+////
+
+//public class Mediator : IMediator
+//{
+//    List<IUzytkownik> uzytkownicy;
+
+//    public Mediator()
+//    {
+//        uzytkownicy = new List<IUzytkownik>();
+//    }
+
+//    public void DodajUzytkownika(IUzytkownik uzytkownik)
+//    {
+//        //
+//        //
+//    }
+
+//    public void WyslijWiadomosc(string wiadomosc, IUzytkownik nadawca)
+//    {
+//        //
+//        //
+//        //
+//    }
+//}
+
+////
+////
+////
+
+//public class Dev : IUzytkownik
+//{
+//    string login;
+//    IMediator Mediator;
+
+//    public Dev(IMediator Mediator, string login)
+//    {
+//        this.login = login;
+//        this.Mediator = Mediator;
+//    }
+
+//    public void WyslijWiadomosc(string wiadomosc)
+//    {
+//        Mediator.WyslijWiadomosc(wiadomosc, this);
+//    }
+
+//    public void OdbierzWiadomosc(string wiadomosc)
+//    {
+//        Console.WriteLine("Programista " + login + " otrzymal wiadomosc: " + wiadomosc);
+//    }
+//}
+
+////
+////
+////
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+
+//        //
+
+//        IUzytkownik ania = new Klient(mediator, "Ania");
+//        //
+//        //
+
+//        //
+//        mediator.DodajUzytkownika(nakamoto);
+//        //
+
+//        //
+//        geohot.WyslijWiadomosc("Czekam az Nakamoto zaparzy kawe...");
+
+//    }
+//}
---
+++ stan/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+interface Stan
+{
+
+    void alert();
+
+}
+
+class Powiadomienia
+{
+
+    private Stan aktualnyStan;
+
+    public Powiadomienia()
+    {
+        aktualnyStan = //domyślny
+  }
+
+    //ustawStan(Stan stan)
+    //
+
+    //
+    //
+
+}
+
+class Dzwonek : Stan
+{
+
+    //
+
+}
+
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //
+        //
+        //
+        //
+        //
+        //
+        powiadomienia.alert();
+        powiadomienia.ustawStan(new Wibracja());
+        powiadomienia.alert();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+
+//interface Stan
+//{
+
+//    void alert();
+
+//}
+
+//class Powiadomienia
+//{
+
+//    private Stan aktualnyStan;
+
+//    public Powiadomienia()
+//    {
+//        aktualnyStan = //domyślny
+//  }
+
+//    //ustawStan(Stan stan)
+//    //
+
+//    //
+//    //
+
+//}
+
+//class Dzwonek : Stan
+//{
+
+//    //
+
+//}
+
+
+
+//class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        //
+//        //
+//        //
+//        //
+//        //
+//        //
+//        powiadomienia.alert();
+//        powiadomienia.ustawStan(new Wibracja());
+//        powiadomienia.alert();
+//    }
+//}
